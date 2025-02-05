0fd71d220837f6cfc45928a7691e02c65787dace
souzan allouch <116023882+12souzan@users.noreply.github.com>
2025-02-05 20:34:18 +0200

    Update CustomPagination.jsx

--- src/component/CustomPagination.jsx
+++ src/component/CustomPagination.jsx
@@ -1,21 +1,24 @@
+import React from 'react';
 import { Pagination, PaginationItem } from "@mui/material";
 import { gridPageCountSelector, gridPageSelector, useGridApiContext, useGridSelector } from "@mui/x-data-grid";
 import { paginationStyle } from "../utils/style";
 
-export default function CustomPagination() {
-    const apiRef = useGridApiContext();
-    const page = useGridSelector(apiRef, gridPageSelector);
-    const pageCount = useGridSelector(apiRef, gridPageCountSelector);
-  
-    return (
-      <Pagination
-        variant="contained"
-        shape="rounded"
-        page={page + 1}
-        count={pageCount}
-        sx={paginationStyle}
-        renderItem={(props2) => <PaginationItem {...props2} disableRipple />}
-        onChange={(event, value) => apiRef.current.setPage(value - 1)}
-      />
-    );
-  }
+function CustomPagination() {
+  const apiRef = useGridApiContext();
+  const page = useGridSelector(apiRef, gridPageSelector);
+  const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+
+  return (
+    <Pagination
+      variant="contained"
+      shape="rounded"
+      page={page + 1}
+      count={pageCount}
+      sx={paginationStyle}
+      renderItem={(props2) => <PaginationItem {...props2} disableRipple />}
+      onChange={(event, value) => apiRef.current.setPage(value - 1)}
+    />
+  );
+}
+
+export default CustomPagination;
